24882f58ea5364749be606ad119ce4a2695b74c2
rudinla <95286204+rudinla@users.noreply.github.com>
2022-08-22 18:33:11 +0700

    Update Coin_Piles_CSES.py

--- Coin_Piles_CSES.py
+++ Coin_Piles_CSES.py
@@ -1,14 +1,4 @@
 for _ in range(int(input())):
     a,b=map(int,input().split())
-    if a>b:
-        k=a-b
-        a-=2*k
-        b-=k
-        if a==b and a>=0 and a%3==0:print('YES')
-        else:print('NO')
-    else:
-        k=b-a
-        b-=2*k
-        a-=k
-        if a==b and a>=0 and a%3==0:print('YES')
-        else:print('NO')
+    if (a+b)%3==0 and max(a,b)<=min(a,b)*2:print('YES')
+    else:print('NO')
